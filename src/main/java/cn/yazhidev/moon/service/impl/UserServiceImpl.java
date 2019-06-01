6c8980968aa1e5fec471ecd1b67043252f6d8301
zengyazhi <zengyazhiwork@163.com>
2019-06-01 12:03:20 +0800

    change

--- src/main/java/cn/yazhidev/moon/service/impl/UserServiceImpl.java
+++ src/main/java/cn/yazhidev/moon/service/impl/UserServiceImpl.java
@@ -4,12 +4,13 @@ import cn.yazhidev.moon.entity.User;
 import cn.yazhidev.moon.mapper.UserMapper;
 import cn.yazhidev.moon.service.UserService;
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
 
-    @Mapper
+    @Autowired
     private UserMapper userMapper;
 
     public User getUserById(int id) {
